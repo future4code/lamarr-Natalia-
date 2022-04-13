dfd2dc66c3c5ac0b0ea61bbfabad4ff6e18023d5
Fulano de Tal <natalia.m@outlook.com.br>
2022-04-12 23:13:22 -0300

    criar variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,37 @@
+
+
+
+
+console.log("Olá", nome, "você tem quantos anos?", idade)
+
+//2. 
+
+
+let qualEstado
+let qualCidade
+let queRua
+
+qualEstado = prompt("Em qual estado você reside?")
+qualCidade = prompt("Em qual cidade?")
+queRua = prompt("Em que rua?")
+
+let São Paulo = qualEstado
+let Avaré = qualCidade
+let Rua Pedro Velho Cardoso = queRua
+
+console.log("Em qual estado você reside?", São Paulo)
+console.log("Em qual cidade?", Avaré)
+console.log("Em que rua?", Rua Pedro Velho Cardoso)
+
+//3. 
+
+let a = 10
+let b = 25
+let c = 0
+c = a
+a = b
+b = c
+
+
+
+
